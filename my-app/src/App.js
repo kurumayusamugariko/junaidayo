e467ac75a37696aba54d2375429860a1c3c3d3bb
kurumayusamugariko <junmomo6780@gmail.com>
2024-02-22 00:39:43 +0900

    firebase連携

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import firebaseApp from "./fire";
+import firestoreDB from "./fire";
+import { collection, getDocs } from "firebase/firestore";
+import AuthComponent from "./AuthComponent";
+import { useEffect, useState } from "react";
 
 function App() {
+  //useState初期設定
+  const [todos, setTodos] = useState([]);
+
+  //データ取得用配列
+  const arrList = [];
+
+  //useEffectの処理
+  useEffect(() => {
+    const fireStorePostData = collection(firestoreDB, "todoposts");
+    getDocs(fireStorePostData).then((snapShot) => {
+      snapShot.forEach((docs) => {
+        const doc = docs.data();
+        arrList.push({
+          id: docs.id,
+          title: doc.title,
+          status: doc.status,
+          shousai: doc.shousai,
+        });
+      });
+      setTodos(arrList);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {todos.map((todo) => {
+					// return(
+          <li key={todo.id}>
+            <div>タイトル：{todo.title}</div>
+            <div>ステータス：{todo.status}</div>
+            <div>詳細：{todo.shousai}</div>
+          </li>
+					// );
+        })}
+      </ul>
+
+      <h1> ログイン機能 </h1>
+      <AuthComponent />
     </div>
   );
 }
