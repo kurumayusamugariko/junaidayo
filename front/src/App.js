2fbbd570de55c1fe8531c1baed1756dcb68a3bd5
hatsune <hatsune.k.pc8010@gmail.com>
2024-02-22 01:04:48 +0900

    first

--- front/src/App.js
+++ front/src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import {useState,useEffect} from 'react'
 
 function App() {
-	const [message, setMessage] = useState('');
-	useEffect(() => {
-		fetch('/sql')
-		.then((res) => res.json())
-		.then((data) => setMessage(data.message));
-	},[])
+	const [message, setMessage] = useState('');//
+	//useEffect(() => {
+		//fetch('/sql')
+		//.then((res) => res.json())
+		//.then((data) => setMessage(data.message));
+	//},[])
   return (
     <div className="App">
-			<h1>フロントエンド</h1>
-			<p>{ message }</p>
+		<h1>フロントエンド</h1>
+			{/* <p>{ message }</p> */}
 		</div>
   );
 }
