8e1775c8ec3e14b27076cabd2d329b8704d2fc23
suzuka215 <109153495+suzuka215@users.noreply.github.com>
2024-03-05 23:57:55 +0900

    Add files via upload

--- front/src/components/Edit.js
+++ front/src/components/Edit.js
@@ -1,14 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Link, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "../css/Edit.css";
+import axios from "axios";
 
 import Button from "@mui/material/Button";
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
+//import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 function Edit() {
-  const navigate = useNavigate();
-  //画像関係
+  const [inputValue, setInputValue] = useState({
+    teamName: "漏瑚対策",
+    1: "1人目",
+    2: "2人目",
+    3: "3人目",
+    4: "4人目",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputValue((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/sql")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  const [textareaValue, setTextareaValue] = useState(message);
+
+  useEffect(() => {
+    setTextareaValue(message);
+  }, [message]);
+
+  const handleTextareaChange = (event) => {
+    setTextareaValue(event.target.value);
+  };
+
   const [imageSrcs, setImageSrcs] = useState({
     member1: "okkotu.jpg",
     member2: "okkotu.jpg",
@@ -31,167 +63,177 @@ function Edit() {
     };
     fileInput.click();
   };
-	console.log("メンバー画像");
-	console.log(imageSrcs);
-  const [memo, setMemo] = useState(""); // memo ステートを追加
 
-
-  //コマンド関係
+  //初期状態
   const [events, setEvents] = useState([
     { type: "wave", index: 0 },
-    { type: "turn", index: 0 },
+    {
+      type: "turn",
+      index: 0,
+      turn1: "1←技A",
+      turn2: "1←技A",
+      turn3: "1←技A",
+      turn4: "1←技A",
+    },
   ]);
+
   const [waveIndex, setWaveIndex] = useState(0);
   const [turnIndex, setTurnIndex] = useState(0);
 
-  
   const newTurn = () => {
-    const turnCount = events.filter(item => item.type === 'turn').length;
-    setEvents([...events, { type: 'turn', index: turnCount, number: turnCount + 1 }]);
+    const turnCount = events.filter((item) => item.type === "turn").length;
+    setEvents([
+      ...events,
+      {
+        type: "turn",
+        index: turnCount,
+        number: turnCount + 1,
+        turn1: "1←技A",
+        turn2: "1←技A",
+        turn3: "1←技A",
+        turn4: "1←技A",
+      },
+    ]);
   };
-  
+
+  const handleSelectChange = (event) => {
+    const selectedOptionText =
+      event.target.options[event.target.selectedIndex].text;
+    const turnProperty = event.target.name;
+    const turnIndex = event.target.getAttribute("data-index");
+
+    setEvents((prevEvents) =>
+      prevEvents.map((event, index) => {
+        if (event.type === "turn" && index === Number(turnIndex)) {
+          return { ...event, [turnProperty]: selectedOptionText };
+        } else {
+          return event;
+        }
+      })
+    );
+  };
+
   const newWave = () => {
     const lastEvent = events[events.length - 1];
-    let waveCount = events.filter(item => item.type === 'wave').length;
-  
-    // 新しい wave イベントを追加する前に、既に存在する wave イベントの数を取得
-    if (lastEvent && lastEvent.type === 'turn') {
-      waveCount = events.filter(item => item.type === 'wave').length;
+    if (lastEvent && lastEvent.type === "turn") {
+      const waveCount = events.filter((item) => (item.type === "wave").length-1);
+      
+      // 新しいWaveが始まる前に既存のWaveの最後のターンがあれば削除
+      if (events.length > 1 && events[events.length - 2].type === "turn") {
+        const newItems = [...events];
+        newItems.splice(newItems.length - 2, 1);
+        setEvents(newItems);
+      }
+      setEvents([
+        ...events,
+       // { type: "wave", index: waveCount, number: waveCount + 1 },
+        { type: "wave", index: waveCount, number: waveCount + 1 },
+      ]);
     }
-  
-    console.log('Adding new wave event:', { type: 'wave', index: waveCount, number: waveCount + 1 });
-  
-    setEvents([...events, { type: 'wave', index: waveCount, number: waveCount + 1 }]);
   };
   
-  
+
   const reset = () => {
     if (window.confirm("本当にリセットしますか？")) {
       setEvents([
         { type: "wave", index: 0 },
         { type: "turn", index: 0 },
-      ]); // eventsを初期状態にリセット
-      setWaveIndex(0); // waveIndexを初期状態にリセット
-      setTurnIndex(0); // turnIndexを初期状態にリセット
+      ]);
+      setWaveIndex(0);
+      setTurnIndex(0);
     }
   };
 
-	const handleDelete = (index) => {
-		const newItems = [...events];
-		newItems.splice(index, 1);
-	
-		let waveCount = 0;
-		let turnCount = 0;
-	
-		setEvents(newItems.map((item, index) => {
-			if (item.type === 'wave') {
-				waveCount += 1;
-				return {...item, index: waveCount - 1, number: waveCount};
-			} else if (item.type === 'turn') {
-				turnCount += 1;
-				return {...item, index: turnCount - 1, number: turnCount};
-			} else {
-				return item;
-			}
-		}));
-	};
+  const handleDelete = (index) => {
+    const newItems = [...events];
+    newItems.splice(index, 1);
 
-  const [selectedOptions, setSelectedOptions] = useState(Array(events.length).fill('firstOption'));
+    let waveCount = 0;
+    let turnCount = 0;
 
-  // handleSelectChange 関数を変更
-  const handleSelectChange = (index, selectedValue) => {
-    setSelectedOptions((prevOptions) => {
-      const newOptions = [...prevOptions];
-      // indexが現在のnewOptionsの長さ以下の場合、そのまま更新、それ以外は新たに追加
-      if (index < newOptions.length) {
-        newOptions[index] = selectedValue;
-      } else {
-        newOptions.push(selectedValue);
-      }
-      return newOptions;
-    });
+    setEvents(
+      newItems.map((item, index) => {
+        if (item.type === "wave") {
+          waveCount += 1;
+          return { ...item, index: waveCount - 1, number: waveCount };
+        } else if (item.type === "turn") {
+          turnCount += 1;
+          return { ...item, index: turnCount - 1, number: turnCount };
+        } else {
+          return item;
+        }
+      })
+    );
   };
+
+  useEffect(() => {
+    console.log("チーム編成");
+    console.log(inputValue);
+    console.log("コマンドの中身");
+    console.log(events);
+    console.log("メンバー画像");
+    console.log(imageSrcs);
+    console.log("メモ");
+    console.log(textareaValue);
+  }, [events, imageSrcs, inputValue, textareaValue]);
+
+
+// 保存成功後にMainpage.jsにリダイレクト
+//const history = useHistory();
+const navigate = useNavigate(); // useNavigateを使ってナビゲーションを管理
+
+  const handleSave = () => {
+    const data = {
+      teamName: inputValue.teamName,
+      members: [
+        { name: inputValue[1], imageSrc: imageSrcs.member1 },
+        { name: inputValue[2], imageSrc: imageSrcs.member2 },
+        { name: inputValue[3], imageSrc: imageSrcs.member3 },
+        { name: inputValue[4], imageSrc: imageSrcs.member4 },
+      ],
+      commands: events.map((event) => {
+        if (event.type === "wave") {
+          return { type: 'wave', index: event.index };
+        } else {
+          return {
+            type: 'turn',
+            index: event.index,
+            number: event.index + 1,
+            text1: event.turn1,
+            text2: event.turn2,
+            text3: event.turn3,
+            text4: event.turn4,
+          };
+        }
+      }),
+      memo: textareaValue,
+    };
   
-
-// 保存ボタンがクリックされた時の処理
-// handleSave 関数の変更
-const handleSave = async (e) => {
-  e.preventDefault();
-
-  try {
-    // チーム名を取得
-    const teamName = document.getElementById('teamName').value;
-
-    // メンバー情報を取得
-    const members = Array.from(document.querySelectorAll('.teamMember li')).map(member => ({
-      name: member.querySelector('input').value,
-      imageSrc: member.querySelector('img').src,
-    }));
-
-    // メモを取得
-    const memo = document.querySelector('.memo textarea').value;
-
-    // コマンド情報を取得
-    const commands = events.map((event, index) => {
-      if (event.type === 'wave') {
-        return { type: 'wave', index: event.index, number: event.number };
-      } else if (event.type === 'turn') {
-        return {
-          type: 'turn',
-          index: event.index,
-          number: event.number,
-          text1: selectedOptions[index * 4] || '',   // デフォルトで空文字列を設定
-          text2: selectedOptions[index * 4 + 1] || '',
-          text3: selectedOptions[index * 4 + 2] || '',
-          text4: selectedOptions[index * 4 + 3] || '',
-        };
-      }
-    });
-
-    console.log("データベースに送信するデータ:", { teamName, members, commands, memo });
-
-    // サーバーにデータを送信
-    const response = await fetch('/teams', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ teamName, members, commands, memo }),
-    });
-
-    // サーバーからのレスポンスをログに出力
-    if (response.ok) {
-      console.log('データが保存されました。');
-      // 画像パスの更新など、必要な処理を追加
-
-      // /main にリダイレクト
-      navigate("/main");
-    } else {
-      console.error('データの保存に失敗しました。', response.status, response.statusText);
-      // エラーレスポンスの内容をログに出力
-      const errorData = await response.json();
-      console.error('エラーレスポンス:', errorData);
-    }
-  } catch (error) {
-    console.error('エラー:', error);
-  }
+    
+  axios.post("/teams", data)
+  .then((response) => {
+    console.log("Data saved", data);
+    //history.push("/main"); // リダイレクト
+    navigate("/main"); // リダイレクト
+  })
+  .catch((error) => {
+    console.error("Error saving data", error);
+  });
 };
-
-  console.log("コマンドの中身"); //コマンドの中身を確認
-	console.log(events);
-
-  
-	useEffect(() => {
-		console.log(events);
-	}, [events]);
 
   return (
     <div className="Mainpage">
       <h1>編集ページ</h1>
-      <form onSubmit={handleSave}>
+      <form method="post">
         <div className="teamName">
           <label htmlFor="teamName">チーム名 :</label>
-          <input type="text" id="teamName" defaultValue="漏瑚対策" />
+          <input
+            type="text"
+            id="teamName"
+            name="teamName"
+            value={inputValue.teamName}
+            onChange={handleInputChange}
+          />
         </div>
 
         <div className="teamMember">
@@ -202,7 +244,14 @@ const handleSave = async (e) => {
                 src={imageSrcs.member1}
                 onClick={() => handleImageUpload("member1")}
               />
-              <input type="text" id="member" defaultValue="東堂 幻" />
+              <input
+                type="text"
+                id="member"
+                name="1"
+                defaultValue={inputValue[1]}
+                value={inputValue.member1}
+                onChange={handleInputChange}
+              />
             </li>
             <li className="m2">
               <img
@@ -210,7 +259,14 @@ const handleSave = async (e) => {
                 src={imageSrcs.member2}
                 onClick={() => handleImageUpload("member2")}
               />
-              <input type="text" id="member" defaultValue="東堂 幻" />
+              <input
+                type="text"
+                id="member"
+                name="2"
+                defaultValue={inputValue[2]}
+                value={inputValue.member2}
+                onChange={handleInputChange}
+              />
             </li>
             <li className="m3">
               <img
@@ -218,7 +274,14 @@ const handleSave = async (e) => {
                 src={imageSrcs.member3}
                 onClick={() => handleImageUpload("member3")}
               />
-              <input type="text" id="member" defaultValue="東堂 幻" />
+              <input
+                type="text"
+                id="member"
+                name="3"
+                defaultValue={inputValue[3]}
+                value={inputValue.member3}
+                onChange={handleInputChange}
+              />
             </li>
             <li className="m4">
               <img
@@ -226,7 +289,14 @@ const handleSave = async (e) => {
                 src={imageSrcs.member4}
                 onClick={() => handleImageUpload("member4")}
               />
-              <input type="text" id="member" defaultValue="東堂 幻" />
+              <input
+                type="text"
+                id="member"
+                name="4"
+                defaultValue={inputValue[4]}
+                value={inputValue.member4}
+                onChange={handleInputChange}
+              />
             </li>
           </ul>
         </div>
@@ -240,15 +310,22 @@ const handleSave = async (e) => {
                     {item.type === "wave" ? (
                       <p className="wave">
                         wave{item.index + 1}
-												<DeleteIcon className="deleteIconW" onClick={() => handleDelete(index)}>Delete</DeleteIcon>
+                        <DeleteIcon
+                          className="deleteIconW"
+                          onClick={() => handleDelete(index)}
+                        >
+                          Delete
+                        </DeleteIcon>
                       </p>
                     ) : (
                       <div className="turn">
-                      <div className="turnNumber">{item.index + 1}</div>
-                      <select
-                        className={`turn turn${item.index}`}
-                        onChange={(e) => handleSelectChange(index, e.target.value)}
-                      >
+                        <div className="turnNumber">{item.index + 1}</div>
+                        <select
+                          className="turn1"
+                          name="turn1"
+                          data-index={index}
+                          onChange={handleSelectChange}
+                        >
                           <option value="firstOption">1←技A</option>
                           <option value="secondOption">1←技B</option>
                           <option value="3rdOption">1←技C</option>
@@ -269,10 +346,12 @@ const handleSave = async (e) => {
                           <option value="15thOption">4←技C</option>
                           <option value="16thOption">4←必</option>
                         </select>
-                       <select
-                        className={`turn turn${item.index}`}
-                        onChange={(e) => handleSelectChange(index, e.target.value)}
-                      >
+                        <select
+                          className="turn2"
+                          name="turn2"
+                          data-index={index}
+                          onChange={handleSelectChange}
+                        >
                           <option value="firstOption">1←技A</option>
                           <option value="secondOption">1←技B</option>
                           <option value="3rdOption">1←技C</option>
@@ -293,10 +372,12 @@ const handleSave = async (e) => {
                           <option value="15thOption">4←技C</option>
                           <option value="16thOption">4←必</option>
                         </select>
-                       <select
-                        className={`turn turn${item.index}`}
-                        onChange={(e) => handleSelectChange(index, e.target.value)}
-                      >
+                        <select
+                          className="turn3"
+                          name="turn3"
+                          data-index={index}
+                          onChange={handleSelectChange}
+                        >
                           <option value="firstOption">1←技A</option>
                           <option value="secondOption">1←技B</option>
                           <option value="3rdOption">1←技C</option>
@@ -317,10 +398,12 @@ const handleSave = async (e) => {
                           <option value="15thOption">4←技C</option>
                           <option value="16thOption">4←必</option>
                         </select>
-                       <select
-                        className={`turn turn${item.index}`}
-                        onChange={(e) => handleSelectChange(index, e.target.value)}
-                      >
+                        <select
+                          className="turn4"
+                          name="turn4"
+                          data-index={index}
+                          onChange={handleSelectChange}
+                        >
                           <option value="firstOption">1←技A</option>
                           <option value="secondOption">1←技B</option>
                           <option value="3rdOption">1←技C</option>
@@ -341,7 +424,12 @@ const handleSave = async (e) => {
                           <option value="15thOption">4←技C</option>
                           <option value="16thOption">4←必</option>
                         </select>
-												<DeleteIcon className="deleteIcon" onClick={() => handleDelete(index)}>Delete</DeleteIcon>
+                        <DeleteIcon
+                          className="deleteIcon"
+                          onClick={() => handleDelete(index)}
+                        >
+                          Delete
+                        </DeleteIcon>
                       </div>
                     )}
                   </li>
@@ -366,14 +454,16 @@ const handleSave = async (e) => {
 
         <div className="memo">
           <p className="contents">memo</p>
-          <textarea value={memo} onChange={(e) => setMemo(e.target.value)}></textarea>
+          <textarea
+            defaultValue={textareaValue}
+            value={textareaValue}
+            onChange={handleTextareaChange}
+          ></textarea>
         </div>
 
-        <Link to="/main">
         <Button type="button" className="toMain" onClick={handleSave}>
-        保存
-      </Button>
-        </Link>
+          保存
+        </Button>
       </form>
     </div>
   );
--- front/src/components/Mainpage.js
+++ front/src/components/Mainpage.js
@@ -1,50 +1,53 @@
+// Mainpage.js
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "../css/Main.css";
-
-// マテリアルUI
 import Button from "@mui/material/Button";
 
 function Mainpage() {
-  const [teamData, setTeamData] = useState({
-    teamName: "漏瑚対策",
-    members: [
-      { name: "東堂 幻", imageSrc: "okkotu.jpg" },
-      { name: "東堂 幻", imageSrc: "okkotu.jpg" },
-      { name: "東堂 幻", imageSrc: "okkotu.jpg" },
-      { name: "東堂 幻", imageSrc: "okkotu.jpg" },
-    ],
-    commands: [
-      {
-        type: "wave",
-        index: 0,
-        number: 1,
-      },
-      {
-        type: "turn",
-        index: 0,
-        number: 1,
-        commands: [
-          { wave: 1, text: "技A" },
-          { wave: 1, text: "技A" },
-          { wave: 1, text: "技A" },
-          { wave: 1, text: "技A" },
-        ],
-      },
-    ],
-    message: "デフォルトメッセージ",
-  });
+  const [teamData, setTeamData] = useState({ team_name: "", memo: ""});
+  console.log("teamData");
+  console.log(teamData);
+  const [memberData, setMemberData] = useState([
+    { id: 1, team_id: 1, name: "", image_path: "okkotu.jpg" },
+    { id: 2, team_id: 1, name: "", image_path: "okkotu.jpg" },
+    { id: 3, team_id: 1, name: "", image_path: "okkotu.jpg" },
+    { id: 4, team_id: 1, name: "", image_path: "okkotu.jpg" },
+  ]);
+  const [commandData, setCommandData] = useState([
+    {
+      id: 1,
+      team_id: 1,
+      wave_number: 0,
+      turn_number: 1,
+      command_text1: "1←技A",
+      command_text2: "1←技A",
+      command_text3: "1←技A",
+      command_text4: "1←技A",
+    },
+  ]);
+  console.log("commandData");
+  console.log(commandData);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("/maindata")
+    fetch("/main/latest") // エンドポイントを修正
       .then((res) => res.json())
-      .then((data) => setTeamData(data));
+      .then((data) => {
+        setTeamData(data.teamData.team_name || {});
+        setMemberData(data.memberData || []);
+        setCommandData(data.commandData || []);
+        setTeamData(data.teamData.memo ? data.teamData.memo : "");
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
 
   return (
     <div className="Mainpage">
       <div className="teamName">
-        <h3>{teamData.teamName}</h3>
+        <h3>{teamData.team_name}</h3>
       </div>
 
       <div className="toEdit">
@@ -54,26 +57,26 @@ function Mainpage() {
       </div>
 
       <div className="teamMember">
-        {teamData.members.map((member, index) => (
-          <img key={index} className={`m${index + 1}`} alt={`メンバー${index + 1}`} src={member.imageSrc} />
+        {memberData.map((member) => (
+          <img key={member.id} alt={member.name} src={member.image_path} />
         ))}
       </div>
 
       <div className="command-container">
-        {teamData.commands.map((command, index) => (
-          <div key={index} className="command">
+        {commandData.map((command) => (
+          <div key={command.id} className="command">
             {command.type === "wave" ? (
-              <div className="wave">{`wave${command.index + 1}`}</div>
-            ) : (
-              <div className="turn-container">
-                <div className="turn">
-                  <div className="turnNumber">{command.number}</div>
-                  {/* ここでコマンド情報の表示を追加 */}
-                  {command.commands.map((cmd, cmdIndex) => (
-                    <div key={cmdIndex} className="t">{`${cmd.wave}←${cmd.text}`}</div>
-                  ))}
-                </div>
+            <div className="wave">wave{command.wave_number + 1}</div>
+            ):(
+            <div className="turn-container">
+              <div className="turn">
+                <div className="turnNumber">{command.turn_number}</div>
+                <div className="first t">{command.command_text1}</div>
+                <div className="second t">{command.command_text2}</div>
+                <div className="third t">{command.command_text3}</div>
+                <div className="forth t">{command.command_text4}</div>
               </div>
+            </div>
             )}
           </div>
         ))}
@@ -81,7 +84,7 @@ function Mainpage() {
 
       <div className="memo">
         <p className="contents">memo</p>
-        <p>{teamData.message}</p>
+        <p>{teamData.memo}</p>
       </div>
 
       <Link to="/" className="link">
