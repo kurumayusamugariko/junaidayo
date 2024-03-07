85c4e5a13f94e5aee578c5cc517eee7f6a0673d5
kurumayusamugariko <junmomo6780@gmail.com>
2024-03-07 23:48:28 +0900

    reset

--- index.js
+++ index.js
@@ -22,12 +22,26 @@ const db = mysql.createPool({
   charset: "utf8mb4",
 });
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   const sqlSelect = "SELECT * FROM Commands ORDER BY id";
 
-  db.query(sqlSelect, (err, result) => {
-    res.send(result);
-  });
+  try {
+    const [result] = await db.promise().query(sqlSelect);
+    res.json(result);
+  } catch (err) {
+		console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+db.getConnection((err, connection) => {
+  if(err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database with thread id ' + connection.threadId);
+  connection.release();
 });
 
 app.get("/api", (req, res) => {
@@ -35,14 +49,19 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/data", async (req, res) => {
-  const [maxTeamId] = await db.promise().query("SELECT MAX(id) as maxId FROM Team");
-  const maxId = maxTeamId[0].maxId;
-	console.log("maxId:", maxId);
-  const [teams] = await db.promise().query("SELECT * FROM Team WHERE id = ?", [maxId]);
-  const [members] = await db.promise().query("SELECT * FROM Members WHERE teamId = ?", [maxId]);
-  const [commands] = await db.promise().query("SELECT * FROM Commands WHERE teamId = ? ORDER BY `id`", [maxId]);
-
-  res.json({ teams, members, commands });
+	try{
+		const [maxTeamId] = await db.promise().query("SELECT MAX(id) as maxId FROM Team");
+		const maxId = maxTeamId[0].maxId;
+		
+		const [teams] = await db.promise().query("SELECT * FROM Team WHERE id = ?", [maxId]);
+		const [members] = await db.promise().query("SELECT * FROM Members WHERE teamId = ?", [maxId]);
+		const [commands] = await db.promise().query("SELECT * FROM Commands WHERE teamId = ? ORDER BY `id`", [maxId]);
+	
+		res.json({ teams, members, commands });
+	}catch(err){
+		res.status(500).json({ error: err.message });
+	}
+  
 });
 
 // to support JSON-encoded bodies
@@ -60,36 +79,37 @@ app.post("/edit", async (req, res) => {
   let result = await query;
   let teamId = result[0].insertId;
 
-  // Insert into Members table
-  for (let i = 1; i <= 4; i++) {
-    let memberData = {
-      teamId: teamId,
-      memberName: data.inputValue[i.toString()],
-      imageSrc: data.imageSrcs["member" + i],
-    };
-    sql = "INSERT INTO Members SET ?";
-    query = db.promise().query(sql, memberData);
-    await query;
-  }
+// Insert into Members table
+let memberPromises = [];
+for (let i = 1; i <= 4; i++) {
+  let memberData = {
+    teamId: teamId,
+    memberName: data.inputValue[i.toString()],
+    imageSrc: data.imageSrcs["member" + i],
+  };
+  sql = "INSERT INTO Members SET ?";
+  query = db.promise().query(sql, memberData);
+  memberPromises.push(query);
+}
+await Promise.all(memberPromises);
 
-  // Insert into Events table
-  for (let event of data.events) {
-    console.log(event.index);
-    let eventData = {
-      teamId: teamId,
-      type: event.type,
-      eventIndex: event.index,
-      turn1: event.turn1,
-      turn2: event.turn2,
-      turn3: event.turn3,
-      turn4: event.turn4,
-    };
-    sql = "INSERT INTO Commands SET ?";
-    query = db.promise().query(sql, eventData);
-    await query;
-  }
-
-  res.json(req.body);
+// Insert into Events table
+let eventPromises = [];
+for (let event of data.events) {
+  let eventData = {
+    teamId: teamId,
+    type: event.type,
+    eventIndex: event.index,
+    turn1: event.turn1,
+    turn2: event.turn2,
+    turn3: event.turn3,
+    turn4: event.turn4,
+  };
+  sql = "INSERT INTO Commands SET ?";
+  query = db.promise().query(sql, eventData);
+  eventPromises.push(query);
+}
+await Promise.all(eventPromises);
 });
 
 app.listen(port, () => {
